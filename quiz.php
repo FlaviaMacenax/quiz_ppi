b856c70b47e098e51aae54b33dd42c1e78064773
Flávia Jamily dos Santos Macena <flavia.jamily.2005@gmail.com>
2023-05-29 16:00:44 -0300

    O começo de um sonho

---
+++ quiz.php
@@ -0,0 +1,35 @@
+<?php
+$pgts = array ("Qual a idade dela?", "Quando ela nasceu?", "Qual o apelido que os fãs brasileiros deram à Taylor Swift?", 
+"Quantos álbuns de estúdio da Taylor Swift foram lançados até agora?", "Qual é o nome do primeiro álbum lançado da cantora?
+", "Qual foi o último relacionamento da Taylor?", "Qual foi o álbum mais vendido da querida?", 
+"Qual é o nome da tour atual (maio/2023) que a Taylor está fazendo?", "Qual é o número da sorte da Srta. Swift?
+", "Quantos grammys ela ganhou?"
+);
+$alternativas = array(
+    array ("27 anos", "30 anos", "33 anos", "36 anos"),
+    array ("13 de dezembro de 1989", "27 de março de 1986", "23 de outubro de 1989", "17 de fevereiro de 1986"),
+    array (),
+    array ("13", "10", "7", "5"),
+    array ("Taylor Swift", "Reputation", "Lover", "Evermore"),
+    array ("Harry Styles", "Joe Alwyn", "Joe Jonas", "Tom Hiddleston"),
+    array ("Folklore", "Red", "1989", "Midnights"),
+    array ("Taylor swift tour", "Evermore tour", "The eras tour", "Midnights tour"),
+    array ("10", "22", "13", "7"),
+    array ("7", "8", "11", "12")
+)
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
