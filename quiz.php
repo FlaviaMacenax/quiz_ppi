b2b81184ad5069c22a3f1627152af894572e378a
Sarah Lima <lima.sarah@email.ifrn.edu.br>
2023-05-29 16:08:22 -0300

    Teste dos arrays

--- quiz.php
+++ quiz.php
@@ -1,14 +1,14 @@
 <?php
 $pgts = array ("Qual a idade dela?", "Quando ela nasceu?", "Qual o apelido que os fãs brasileiros deram à Taylor Swift?", 
-"Quantos álbuns de estúdio da Taylor Swift foram lançados até agora?", "Qual é o nome do primeiro álbum lançado da cantora?
-", "Qual foi o último relacionamento da Taylor?", "Qual foi o álbum mais vendido da querida?", 
+"Quantos álbuns de estúdio da Taylor foram lançados até agora?", "Qual é o nome do primeiro álbum da cantora?
+", "Qual foi o último relacionamento da Taylor?", "Qual foi o álbum mais vendido da Taylor?", 
 "Qual é o nome da tour atual (maio/2023) que a Taylor está fazendo?", "Qual é o número da sorte da Srta. Swift?
 ", "Quantos grammys ela ganhou?"
 );
 $alternativas = array(
     array ("27 anos", "30 anos", "33 anos", "36 anos"),
     array ("13 de dezembro de 1989", "27 de março de 1986", "23 de outubro de 1989", "17 de fevereiro de 1986"),
-    array (),
+    array ("Loirinha", "Loirona", "Lourena", "Loucona"),
     array ("13", "10", "7", "5"),
     array ("Taylor Swift", "Reputation", "Lover", "Evermore"),
     array ("Harry Styles", "Joe Alwyn", "Joe Jonas", "Tom Hiddleston"),
@@ -27,7 +27,7 @@ $alternativas = array(
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Quiz Taylor Swift</title>
 </head>
 <body>
     
