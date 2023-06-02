0f241f461eac6b068fa24e70df49dbf527bef410
Flávia Jamily dos Santos Macena <flavia.jamily.2005@gmail.com>
2023-06-02 09:58:58 -0300

    Iniciando

--- quiz.php
+++ quiz.php
@@ -1,11 +1,11 @@
 <?php
-$pgts = array ("Qual a idade dela?", "Quando ela nasceu?", "Qual o apelido que os fãs brasileiros deram à Taylor Swift?", 
+$perguntas = array ("Qual a idade dela?", "Quando ela nasceu?", "Qual o apelido que os fãs brasileiros deram à Taylor Swift?", 
 "Quantos álbuns de estúdio da Taylor foram lançados até agora?", "Qual é o nome do primeiro álbum da cantora?
 ", "Qual foi o último relacionamento da Taylor?", "Qual foi o álbum mais vendido da Taylor?", 
 "Qual é o nome da tour atual (maio/2023) que a Taylor está fazendo?", "Qual é o número da sorte da Srta. Swift?
 ", "Quantos grammys ela ganhou?"
 );
-$alternativas = array(
+$respostas = array(
     array ("27 anos", "30 anos", "33 anos", "36 anos"),
     array ("13 de dezembro de 1989", "27 de março de 1986", "23 de outubro de 1989", "17 de fevereiro de 1986"),
     array ("Loirinha", "Loirona", "Lourena", "Loucona"),
@@ -20,16 +20,24 @@ $alternativas = array(
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz Taylor Swift</title>
+    <title>Quiz - Taylor Swift</title>
 </head>
 <body>
-    
+
+    <h1>Pergunta 1</h1>
+    <h2><?php echo $perguntas[0]?></h2>
+
+    <form action="quiz_ppi" method="post">
+    <input type="radio" name="respts" value="1"><?php echo($respostas[0][0])?></br>
+    <input type="radio" name="respts" value="2"><?php echo($respostas[0][1])?></br>
+    <input type="radio" name="respts" value="3"><?php echo($respostas[0][2])?></br>
+    <input type="radio" name="respts" value="4"><?php echo($respostas[0][3])?></br> 
+    </form>
 </body>
 </html>
