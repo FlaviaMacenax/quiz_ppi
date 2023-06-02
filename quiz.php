d88a36291e296fc442f5dab471ee03b2c1d7092f
Flávia Jamily dos Santos Macena <flavia.jamily.2005@gmail.com>
2023-06-02 17:30:27 -0300

    aff

--- quiz.php
+++ quiz.php
@@ -1,7 +1,7 @@
 <?php
-$perguntas = array ("Qual a idade dela?", "Quando ela nasceu?", "Qual o apelido que os fãs brasileiros deram à Taylor Swift?", 
+$perguntas = array ("Qual a idade dela?", "Quando ela nasceu?", "Qual o apelido que os fãs brasileiros deram à Taylor Swift?",
 "Quantos álbuns de estúdio da Taylor foram lançados até agora?", "Qual é o nome do primeiro álbum da cantora?
-", "Qual foi o último relacionamento da Taylor?", "Qual foi o álbum mais vendido da Taylor?", 
+", "Qual foi o último relacionamento da Taylor?", "Qual foi o álbum mais vendido da Taylor?",
 "Qual é o nome da tour atual (maio/2023) que a Taylor está fazendo?", "Qual é o número da sorte da Srta. Swift?
 ", "Quantos grammys ela ganhou?"
 );
@@ -16,9 +16,32 @@ $respostas = array(
     array ("Taylor swift tour", "Evermore tour", "The eras tour", "Midnights tour"),
     array ("10", "22", "13", "7"),
     array ("7", "8", "11", "12")
-)
+);
+
+
+$posi = 0;
+$msg = "";
+$msg = "";
+
+
+if(isset($_POST["responder"])){
+    $botao = $_POST["responder"];
+
+
+        if($_POST["respostas"] == "c"){
+            $msg = "Resposta Correta. Parabéns!";
+        }else{
+            $msg = "Resposta Incorreta. Tente novamente!";
+        }
+
+
+}
+
 
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -30,14 +53,133 @@ $respostas = array(
 </head>
 <body>
 
+
+    <form action="quiz.php" method="post">
     <h1>Pergunta 1</h1>
     <h2><?php echo $perguntas[0]?></h2>
+    <input type="radio" name="respostas" value="e"><?php echo($respostas[0][0])?></br>
+    <input type="radio" name="respostas" value="e"><?php echo($respostas[0][1])?></br>
+    <input type="radio" name="respostas" value="e"><?php echo($respostas[0][2])?></br>
+    <input type="radio" name="respostas" value="c"><?php echo($respostas[0][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    <?php echo $msg?>
+    </form>
+   
 
-    <form action="quiz_ppi" method="post">
-    <input type="radio" name="respts" value="1"><?php echo($respostas[0][0])?></br>
-    <input type="radio" name="respts" value="2"><?php echo($respostas[0][1])?></br>
-    <input type="radio" name="respts" value="3"><?php echo($respostas[0][2])?></br>
-    <input type="radio" name="respts" value="4"><?php echo($respostas[0][3])?></br> 
+
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 2</h1>
+    <h2><?php echo $perguntas[1]?></h2>
+    <input type="radio" name="respostas" value="e"><?php echo($respostas[1][0])?></br>
+    <input type="radio" name="respostas" value="c"><?php echo($respostas[1][1])?></br>
+    <input type="radio" name="respostas" value="e"><?php echo($respostas[1][2])?></br>
+    <input type="radio" name="respostas" value="e"><?php echo($respostas[1][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    <?php echo $msg?>
+    </form>
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 3</h1>
+    <h2><?php echo $perguntas[2]?></h2>
+    <input type="radio" name="respostas" value="0"><?php echo($respostas[2][0])?></br>
+    <input type="radio" name="respostas" value="1"><?php echo($respostas[2][1])?></br>
+    <input type="radio" name="respostas" value="2"><?php echo($respostas[2][2])?></br>
+    <input type="radio" name="respostas" value="3"><?php echo($respostas[2][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    </form>
+
+
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 4</h1>
+    <h2><?php echo $perguntas[3]?></h2>
+    <input type="radio" name="respostas" value="0"><?php echo($respostas[3][0])?></br>
+    <input type="radio" name="respostas" value="1"><?php echo($respostas[3][1])?></br>
+    <input type="radio" name="respostas" value="2"><?php echo($respostas[3][2])?></br>
+    <input type="radio" name="respostas" value="3"><?php echo($respostas[3][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    </form>
+
+
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 5</h1>
+    <h2><?php echo $perguntas[4]?></h2>
+    <input type="radio" name="respostas" value="0"><?php echo($respostas[4][0])?></br>
+    <input type="radio" name="respostas" value="1"><?php echo($respostas[4][1])?></br>
+    <input type="radio" name="respostas" value="2"><?php echo($respostas[4][2])?></br>
+    <input type="radio" name="respostas" value="3"><?php echo($respostas[4][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    </form>
+
+
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 6</h1>
+    <h2><?php echo $perguntas[5]?></h2>
+    <input type="radio" name="respostas" value="0"><?php echo($respostas[5][0])?></br>
+    <input type="radio" name="respostas" value="1"><?php echo($respostas[5][1])?></br>
+    <input type="radio" name="respostas" value="2"><?php echo($respostas[5][2])?></br>
+    <input type="radio" name="respostas" value="3"><?php echo($respostas[5][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    </form>
+
+
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 7</h1>
+    <h2><?php echo $perguntas[6]?></h2>
+    <input type="radio" name="respostas" value="0"><?php echo($respostas[6][0])?></br>
+    <input type="radio" name="respostas" value="1"><?php echo($respostas[6][1])?></br>
+    <input type="radio" name="respostas" value="2"><?php echo($respostas[6][2])?></br>
+    <input type="radio" name="respostas" value="3"><?php echo($respostas[6][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    </form>
+
+
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 8</h1>
+    <h2><?php echo $perguntas[7]?></h2>
+    <input type="radio" name="respostas" value="0"><?php echo($respostas[7][0])?></br>
+    <input type="radio" name="respostas" value="1"><?php echo($respostas[7][1])?></br>
+    <input type="radio" name="respostas" value="2"><?php echo($respostas[7][2])?></br>
+    <input type="radio" name="respostas" value="3"><?php echo($respostas[7][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    </form>
+
+
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 9</h1>
+    <h2><?php echo $perguntas[8]?></h2>
+    <input type="radio" name="respostas" value="0"><?php echo($respostas[8][0])?></br>
+    <input type="radio" name="respostas" value="1"><?php echo($respostas[8][1])?></br>
+    <input type="radio" name="respostas" value="2"><?php echo($respostas[8][2])?></br>
+    <input type="radio" name="respostas" value="3"><?php echo($respostas[8][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
+    </form>
+
+
+
+
+    <form action="quiz.php" method="post">
+    <h1>Pergunta 10</h1>
+    <h2><?php echo $perguntas[9]?></h2>
+    <input type="radio" name="respostas" value="0"><?php echo($respostas[9][0])?></br>
+    <input type="radio" name="respostas" value="1"><?php echo($respostas[9][1])?></br>
+    <input type="radio" name="respostas" value="2"><?php echo($respostas[9][2])?></br>
+    <input type="radio" name="respostas" value="3"><?php echo($respostas[9][3])?></br>
+    <input type="submit" name="responder" class="botao" value="Responder">
     </form>
 </body>
 </html>
