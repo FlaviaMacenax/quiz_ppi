725b22f0970f9c01f6bc8a8efe4209adbd39f6a8
Sarah  Lima <lima.sarah@escolar.ifrn.edu.br>
2023-06-02 18:00:35 -0300

    Atualizações na página inicial e na página de instruções

---
+++ instruções.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon"  type="image/x-icon" href="imagens/logo.png">
+    <link rel="stylesheet" type="text/css" href="estilo_quiz.css" />
+    <title>Instruções</title>
+</head>
+<body>
+    <!-- Cabeçalho -->
+    <header class="container">
+      <div class="logo">
+        <img src="imagens/logo.png" width="200" height="106" alt="logo do quiz">
+      </div> 
+    </header>
+
+    <!-- Botão voltar-->
+    <section class="menu contpag">
+    <div class="voltar">
+        <a href="inicial.php">Voltar</a>
+    </div>
+</section>
+
+<!-- Instruções -->
+<section class="contpag">
+                <h2>Como jogar?</h2>
+                <img src="imagens/iconeInstr.png" width="230px" height="auto" alt="ícone de instruções">
+                 <p> O Taylor Swift Quiz é um jogo de perguntas e respostas, onde para cada pergunta lhe são fornecidas quatro 
+                    alternativas mas apenas uma é correta. Você deve descobrir qual opção é a correta para conseguir acumular 
+                    a maior quantidade de pontos. Para cada resposta certa você ganha 100 pontos e uma resposta errada não lhe 
+                    faz ganhar pontos. Será que você é capaz de chegar aos mil pontos ?
+                </p>
+    </section>
+
+</body>
+</html>
