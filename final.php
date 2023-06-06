dba393b26688a3f7ce8065e667c1211f6d54f3b0
Sarah  Lima <lima.sarah@escolar.ifrn.edu.br>
2023-06-06 10:08:33 -0300

    Alteraçãozinha na página final

--- final.php
+++ final.php
@@ -8,45 +8,45 @@ $txtPts = "";
 // Mensagens para acertos:
 
 if ($_SESSION["acertos"] == 0) {
-    $txtActs = "Que pena! Você acertou " . echo $_SESSION["acertos"] . " de 10 questões ";
+    $txtActs = "Que pena! Você não acertou nenhuma das questões";
 }
 
-if (0 < $_SESSION["acertos"] >= 5) {
-    $txtActs = "Boa tentativa, mas você acertou " . echo $_SESSION["acertos"] . " de 10 questões ";
+if ($_SESSION["acertos"] >= 3) {
+    $txtActs = "Boa tentativa, mas você acertou " . echo $_SESSION["acertos"] . " das 10 questões ";
 }
 
-if (5 < $_SESSION["acertos"] >= 7) {
-    $txtActs = "Olha só! Você acertou " . echo $_SESSION["acertos"] . " de 10 questões ";
+if ($_SESSION["acertos"] >= 6) {
+    $txtActs = "Olha só! Você acertou " . echo $_SESSION["acertos"] . " das 10 questões ";
 }
 
-if (7 < $_SESSION["acertos"] >= 9) {
-    $txtActs = "Uau! Você acertou " . echo $_SESSION["acertos"] . "de 10 questões ";
+if ($_SESSION["acertos"] >= 9) {
+    $txtActs = "Uau! Você acertou " . echo $_SESSION["acertos"] . "das 10 questões ";
 }
 
 if ($_SESSION["acertos"] == 10) {
-    $txtActs = "Parabéns!! Você acertou " . echo $_SESSION["acertos"] . "de 10 questões ";
+    $txtActs = "Parabéns!! Você acertou todas as questões";
 }
 
 //Mensagens para pontos:
 
 if ($_SESSION["pontos"] == 0) {
-    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Tente novamente!";
+    $txtPts = "e não ganhou nenhum ponto. Tente novamente!";
 }
 
-if (0 < $_SESSION["pontos"] >= 500) {
+if (0 < $_SESSION["pontos"] >= 300) {
     $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você ainda pode melhorar se tentar novamente!";
 }
 
-if (500 < $_SESSION["pontos"] >= 700) {
+if (500 < $_SESSION["pontos"] >= 600) {
     $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você está indo bem!";
 }
 
 if (700 < $_SESSION["pontos"] >= 900) {
-    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você foi ótima!";
+    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você foi ótimo/a!";
 }
 
 if ($_SESSION["pontos"] == 1000) {
-    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . "pontos. Você é uma verdadeira Swifter!";
+    $txtPts = "e acumulou 100 pontos. Você é um/a verdadeiro/a Swifter!";
 }
 
 ?>
