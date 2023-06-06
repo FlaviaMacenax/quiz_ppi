c6513c083a5fc139516363337f7c41d4e83faeaa
Flávia Jamily dos Santos Macena <flavia.jamily.2005@gmail.com>
2023-06-06 09:53:24 -0300

    Não sei programasr, bjs da Flavinha

--- final.php
+++ final.php
@@ -12,41 +12,41 @@ if ($_SESSION["acertos"] == 0) {
 }
 
 if (0 < $_SESSION["acertos"] >= 5) {
-    $txtActs = "Boa tentativa, mas você acertou " . echo $_SESSION["acertos"] . " de 10 questões "
+    $txtActs = "Boa tentativa, mas você acertou " . echo $_SESSION["acertos"] . " de 10 questões ";
 }
 
 if (5 < $_SESSION["acertos"] >= 7) {
-    $txtActs = "Olha só! Você acertou " . echo $_SESSION["acertos"] . " de 10 questões "
+    $txtActs = "Olha só! Você acertou " . echo $_SESSION["acertos"] . " de 10 questões ";
 }
 
 if (7 < $_SESSION["acertos"] >= 9) {
-    $txtActs = "Uau! Você acertou " . echo $_SESSION["acertos"] . "de 10 questões "
+    $txtActs = "Uau! Você acertou " . echo $_SESSION["acertos"] . "de 10 questões ";
 }
 
 if ($_SESSION["acertos"] == 10) {
-    $txtActs = "Parabéns!! Você acertou " . echo $_SESSION["acertos"] . "de 10 questões "
+    $txtActs = "Parabéns!! Você acertou " . echo $_SESSION["acertos"] . "de 10 questões ";
 }
 
 //Mensagens para pontos:
 
 if ($_SESSION["pontos"] == 0) {
-    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Tente novamente!"
+    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Tente novamente!";
 }
 
 if (0 < $_SESSION["pontos"] >= 500) {
-    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você ainda pode melhorar se tentar novamente!"
+    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você ainda pode melhorar se tentar novamente!";
 }
 
 if (500 < $_SESSION["pontos"] >= 700) {
-    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você está indo bem!"
+    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você está indo bem!";
 }
 
 if (700 < $_SESSION["pontos"] >= 900) {
-    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você foi ótima!"
+    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . " pontos. Você foi ótima!";
 }
 
 if ($_SESSION["pontos"] == 1000) {
-    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . "pontos. Você é uma verdadeira Swifter!"
+    $txtPts = "e acumulou " . echo $_SESSION["pontos"] . "pontos. Você é uma verdadeira Swifter!";
 }
 
 ?>
--- original.php
+++ original.php
@@ -2,7 +2,7 @@
 
 session_start();
     $valor = 0;
-    $msg=0;
+    $msg= "";
     $pontos="";
     $acertos = "";
 
@@ -28,20 +28,16 @@ session_start();
 
 
     function exibirQuestao($i){
-        global $perguntas, $respostas, $_SESSION;
+        global $perguntas, $respostas;
 ?>
-    <section id="pergunta-1">
         <form action="original.php" method="post"><br>
         <h1>Pergunta <?php echo $i+1?></h1>
         <h2><?php echo $perguntas[$i]?></h2>
-        <input type="radio" name="res1" value="27"><?php echo($respostas[$i][0])?></br>
-        <input type="radio" name="res1" value="30"><?php echo($respostas[$i][1])?></br>
-        <input type="radio" name="res1" value="33"><?php echo($respostas[$i][2])?></br>
-        <input type="radio" name="res1" value="36"><?php echo($respostas[$i][3])?></br>
-        <input type="submit" name="responder" class="botao" value="Responder">
+        <input type="radio" name="res" value="0"><?php echo($respostas[$i][0])?></br>
+        <input type="radio" name="res" value="1"><?php echo($respostas[$i][1])?></br>
+        <input type="radio" name="res" value="2"><?php echo($respostas[$i][2])?></br>
+        <input type="radio" name="res" value="3"><?php echo($respostas[$i][3])?></br>
         </form>
-    </section>
-
 <?php
     }   
 //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACHORO
@@ -52,43 +48,54 @@ session_start();
         }else{
             $pontos= $pontos+0;
             $acertos= $acertos+0;
+            
+        }
+    }*/
+    /*if(isset($_POST["responder"])){
+        $botao = $_POST["responder"];
+        if(isset($_POST[$perguntas[0]])){
+            if(($_POST[$perguntas[0]], $respostas[0][2]));
+            }$msg="Parabéns, você acertou!";
+       
+        }*/
+
+
+    if(isset($_POST["responder"])){
+        $botao = $_POST["responder"];
+    if(isset($_POST[$perguntas[1]])){
+        if($_POST[$perguntas[1]] == $respostas[1][2]) {
+            $msg = "Parabéns, você acertou!";
+            $pontos = $pontos + 100;
+            $acertos = $acertos + 1;
+        }else{
+            $msg = "Que pena, você errou!";
+            $pontos = $pontos + 0;
+            $acertos = $acertos + 0;
         }
     }
-        if(isset($_POST["responder"])){
-            $botao = $_POST["responder"];
-            //validação das respostas
-            if(isset($_POST["res1"])){
-                if(validarResposta($_POST["res1"], "33")) {
-                    $msg = "Parabéns, você acertou!";
-                }else{
-                    $msg = "Que pena, você errou!";
-                    
-                }
-            }
+}
 
-    }*/
+
+
+
 
     if(isset($_POST['num'])){
         $valor = $_POST['num'];
     }
 
-    if(isset($_POST['botao1'])){
-        $valor = $_POST['num'] - 1;
-    }
-
     if(isset($_POST['botao2'])){
         $valor = $_POST['num'] + 1;
     }
-    if(isset($_POST['botao1']) && $valor = $_POST['num'] < 1){
+    /*if(isset($_POST['botao2']) && $valor = $_POST['num'] > 10){
         ;
-    }
+    }*/
 
 
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -97,16 +104,12 @@ session_start();
 </head>
 <body>
     <h3><?php echo "Jogador: " . $_SESSION["nomeJogador"];?></h3>
-    <?php
+    <?php exibirQuestao($valor);?><br>
 
-        exibirQuestao($valor);
-    ?>
     <form action="original.php" method="post">
         <input type="hidden" name="num" value="<?php echo $valor ?>">
-        <button id="botao" name="botao1" value="-">Anterior</button>
-
+        <button name="responder" value="responder">Responder</button><br>
         <button name="botao2" value="+">Próxima</button>
     </form>
-
 </body>
 </html>
