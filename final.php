548711b4ecd96ad1e7d8e11ebb4d43abe54f46d1
Flávia Jamily dos Santos Macena <flavia.jamily.2005@gmail.com>
2023-06-13 23:09:40 -0300

    Gloria aleluia, irmãs

--- final.php
+++ final.php
@@ -12,15 +12,15 @@ if ($_SESSION["acertos"] == 0) {
 }
 
 if ($_SESSION["acertos"] >= 1 && $_SESSION["acertos"] <= 4) {
-    $txtActs = "Olha só! Você acertou " . $_SESSION["acertos"] . " questões de 10 ";
+    $txtActs = "Olha só! Você acertou " . $_SESSION["acertos"]+1 . " questões de 10 ";
 }
 
 if ($_SESSION["acertos"] >= 5 && $_SESSION["acertos"] <= 7) {
-    $txtActs = "Uau! Você acertou " . $_SESSION["acertos"] . " questões de 10 ";
+    $txtActs = "Uau! Você acertou " . $_SESSION["acertos"]+1 . " questões de 10 ";
 }
 
 if ($_SESSION["acertos"] >= 8 && $_SESSION["acertos"] <= 10) {
-    $txtActs = "Parabéns!! Você acertou " . $_SESSION["acertos"] . " questões de 10 ";
+    $txtActs = "Parabéns!! Você acertou " . $_SESSION["acertos"]+1 . " questões de 10 ";
 }
 
 //Mensagens para pontos:
@@ -30,15 +30,15 @@ if ($_SESSION["ponto"] == 0) {
 }
 
 if ($_SESSION["ponto"] >= 100 && $_SESSION["ponto"] <= 400) {
-    $txtPts = "e acumulou " . $_SESSION["ponto"] . " pontos. Você foi bem, mas pode se dar melhor se tentar novamente!";
+    $txtPts = "e acumulou " . $_SESSION["ponto"]+100 . " pontos. Você foi bem, mas pode se dar melhor se tentar novamente!";
 }
 
 if ($_SESSION["ponto"] >= 500 && $_SESSION["ponto"] <= 700) {
-    $txtPts = "e acumulou " . $_SESSION["ponto"] . " pontos. Você foi ótimo(a)!";
+    $txtPts = "e acumulou " . $_SESSION["ponto"]+100 . " pontos. Você foi ótimo(a)!";
 }
 
 if ($_SESSION["ponto"] >= 800 && $_SESSION["ponto"] <= 1000) {
-    $txtPts = "e acumulou " . $_SESSION["ponto"] . " pontos. Você é um(a) verdadeiro(a) Swifter!";
+    $txtPts = "e acumulou " . $_SESSION["ponto"]+100 . " pontos. Você é um(a) verdadeiro(a) Swifter!";
 }
 ?>
 
